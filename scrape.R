0bda521f75a123040c6cb43a9fc6637b13fcf802
bayuparamita <111562500+bayuparamita@users.noreply.github.com>
2023-06-09 14:01:16 +0700

    Add files via upload
    syntax r

---
+++ scrape.R
@@ -0,0 +1,28 @@
+# Set library ----
+message("Load the libraries")
+library(rvest)
+library(tidyverse)
+library(mongolite)
+
+message("Define function to scrape OP character")
+url<-"https://bwf.tournamentsoftware.com/ranking/ranking.aspx?rid=70"
+html<-read_html(url)
+h1<-html %>% html_nodes("table") %>% html_table()
+h2<-as.data.frame(h1[[1]])
+player<-h2[c(3:12,17:26,31:40,45:54, 59:68),c(1,5,8,11)]
+kategori<-c(rep("Tunggal Putra", 10),rep("Tunggal Putri", 10), rep("Ganda Putra", 10), rep("Ganda Putri", 10), rep("Ganda Campuran", 10))
+colnames(player)<- c("Men's Singles" = "Ranking", "Men's Singles.1" = "Nama", "Men's Singles.2" = "Skor", "More" = "Negara")
+pemain<-player %>% mutate(kategori)
+
+message("Connect to MongoDB Cloud")
+#MongoDB
+atlas <- mongo(
+  collection = "badminton",
+  db         = "em_de_es",
+  url        = "mongodb+srv://user:33wqFbSy1WshrqSt@cluster0.g1kq9iu.mongodb.net/?retryWrites=true&w=majority"
+)
+
+message("Store data frame into mongo cloud")
+atlas$insert(pemain)
+
+atlas$disconnect()
