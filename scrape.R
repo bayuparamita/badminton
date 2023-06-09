8a7231028bd60cd6edcf2dfd3842f92cae469a19
bayuparamita <111562500+bayuparamita@users.noreply.github.com>
2023-06-09 20:52:10 +0700

    Update scrape.R

--- scrape.R
+++ scrape.R
@@ -10,9 +10,9 @@ html<-read_html(url)
 h1<-html %>% html_nodes("table") %>% html_table()
 h2<-as.data.frame(h1[[1]])
 player<-h2[c(3:12,17:26,31:40,45:54, 59:68),c(1,5,8,11)]
-kategori<-c(rep("Tunggal Putra", 10),rep("Tunggal Putri", 10), rep("Ganda Putra", 10), rep("Ganda Putri", 10), rep("Ganda Campuran", 10))
+Kategori<-c(rep("Tunggal Putra", 10),rep("Tunggal Putri", 10), rep("Ganda Putra", 10), rep("Ganda Putri", 10), rep("Ganda Campuran", 10))
 colnames(player)<- c("Men's Singles" = "Ranking", "Men's Singles.1" = "Nama", "Men's Singles.2" = "Skor", "More" = "Negara")
-pemain<-player %>% mutate(kategori)
+pemain<-player %>% mutate(Kategori)
 
 #MongoDB
 message("Connect to MongoDB Cloud")
